0ce1b25c5ec86cceff2bd36ea2106f6d3a8bc4cc
depauladiasleo <109622859+depauladiasleo@users.noreply.github.com>
2022-12-22 10:35:28 -0300

    Update W42.R
    Rework the plot's contrast and readability.
    
    Minor improvement of code readability also.

--- Week_42/W42.R
+++ Week_42/W42.R
@@ -119,68 +119,89 @@ df |>
        caption = títulos$legenda)
 
 
-# # Plot 2 ----------------------------------------------------------------
+## Plot 2 ----------------------------------------------------------------
 
 
-episodes_df |> 
-  mutate(season_lbl = case_when(
-    season == 1 ~ "1st Season",
-    season == 2 ~ "2nd Season",
-    season == 3 ~ "3rd Season",
-    season == 4 ~ "4th Season",
-  )) |> 
+episodes_df |>
+  mutate(
+    season_lbl = case_when(
+      season == 1 ~ "1st Season",
+      season == 2 ~ "2nd Season",
+      season == 3 ~ "3rd Season",
+      season == 4 ~ "4th Season",
+    )
+  ) |>
   ggplot(aes(y = written_by)) +
-  geom_bar(fill = "#2C1414",
-           color = "#616E7C",
-           size = 0.1,
+  geom_bar(fill = txt_col,
+           #color = "#CC5C5C",
+           #size = 0.25,
            width = 0.75) +
-  #scale_fill_manual(name = "Writers, as presented in the stream", 
+  #scale_fill_manual(name = "Writers, as presented in the stream",
   #                  values = carto.pal("wine.pal")) +
   theme_void() +
   theme(
-    panel.grid.major.x = element_line(linetype = 3,
-                                      size = 0.25,
-                                      color = "#616E7C"),
-    axis.text.x = element_text(family = "Segoe UI",
-                               size = 10, 
-                               color = "#616E7C"),
-    axis.text.y = element_text(family = "Segoe UI",
-                               size = 10, 
-                               color = "#616E7C",
-                               hjust = 1),
-    axis.title.x = element_text(family = "Segoe UI",
-                                size = 10, 
-                                color = "white",
-                                #  face = "bold",
-                                hjust = 1),
+    panel.grid.major.x = element_line(
+      linetype = 3,
+      size = 0.25,
+      color = txt_col
+    ),
+    axis.text.x = element_text(
+      family = "Segoe UI",
+      size = 12,
+      color = txt_col
+    ),
+    axis.text.y = element_text(
+      family = "Segoe UI",
+      size = 12,
+      color = txt_col,
+      hjust = 1
+    ),
+    axis.title.x = element_text(
+      family = "Segoe UI",
+      size = 12,
+      color = white,
+      #  face = "bold",
+      hjust = 1
+    ),
     plot.background = element_rect(fill = bg_color,
                                    color = bg_color),
-    plot.title = element_text(family = "Segoe UI",
-                              size = 24,
-                              color = "gray95",
-                              face = "bold"),
-    plot.subtitle = element_text(family = "Segoe_UI",
-                                 size = 17,
-                                 color = "#616E7C",
-                                 margin = margin(6, 0, 36, 0, "pt")),
+    plot.title = element_text(
+      family = "Segoe UI",
+      size = 24,
+      color = white,
+      face = "bold"
+    ),
+    plot.subtitle = element_text(
+      family = "Segoe_UI",
+      size = 17,
+      color = txt_col,
+      margin = margin(6, 0, 36, 0, "pt")
+    ),
     plot.title.position = "plot",
-    plot.caption = element_text(family = "Segoe_UI",
-                                size = 14,
-                                color = "#616E7C",
-                                margin = margin(36, 0, 0, 0, "pt"),
-                                hjust = 0.5),
+    plot.caption = element_text(
+      family = "Segoe_UI",
+      size = 14,
+      color = txt_col,
+      margin = margin(36, 0, 0, 0, "pt"),
+      hjust = 0.5
+    ),
     plot.caption.position = "plot",
     plot.margin = margin(2, 2, 2, 2, "cm"),
     legend.position = "none",
-    strip.text = element_text(family = "Segoe_UI",
-                              face = "bold",
-                              size = 12,
-                              color = "#616E7C",
-                              hjust = 0,
-                              margin = margin(0, 0, 12, 0, "pt")) ) +
-  labs(title = títulos$título,
-       subtitle = títulos$subtítulo,
-       caption = títulos$legenda) +
+    strip.text = element_text(
+      family = "Segoe_UI",
+      face = "bold",
+      size = 14,
+      color = txt_col,
+      hjust = 0,
+      margin = margin(0, 0, 12, 0, "pt")
+    )
+  ) +
+  labs(
+    title = títulos$título,
+    subtitle = títulos$subtítulo,
+    caption = títulos$legenda
+  ) +
   facet_wrap("season_lbl", ncol = 1,
              scales = "free_y")
 
@@ -189,6 +210,13 @@ episodes_df |>
 # Exportação ---------------------------------------------------
 
 
-ggsave("TidyTuesday42.png", 
-       width = 6741, height = 3792, dpi = 320, units = "px")
+ggsave(
+  "TidyTuesday42.png",
+  width = 6741,
+  height = 3792,
+  dpi = 320,
+  units = "px"
+)
+
+
 
